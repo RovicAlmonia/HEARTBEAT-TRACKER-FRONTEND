5f749a2a0eb2b5560da72ec3092754011fc1acf1
Rovic Matthew Almonia <160310360+RovicAlmonia@users.noreply.github.com>
2025-05-16 14:52:20 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.7:8000', // Change if needed
+  baseURL: 'https://heartbeat-tracker-backend.onrender.com/', // Change if needed
 });
 
 // Attach token to all requests
